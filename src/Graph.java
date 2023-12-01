f2fd3cd7c77ea664bb5b5bc3530d0e4646ff7a31
zach <zgarson@u.rochester.edu>
2023-11-30 20:10:18 -0500

    created weighting system for graph

--- src/Graph.java
+++ src/Graph.java
@@ -1,12 +1,15 @@
 
 public class Graph {
 
-    public HashTable<String, Vertex> adjList; // ajdacency list
+    public HashTable<String, Vertex> adjList; // adjacency list
     public int edges;
     public int vertices; // track the number of vertices added to the graph
 
+    public int matrix[][];
+    boolean visited[][];
 
-    public Heap<Edge> pq = new Heap<Edge>();
+
+    public Heap<Edge> pq = new Heap<>();
 
     // graph constructor
     Graph(){
@@ -20,11 +23,15 @@ public class Graph {
         vertices++;
     }
 
+    public void addVertex(Vertex vertex){
+
+    }
+
     public static void main(String[] args){
-        Vertex v1 = new Vertex("v1", 13.5, 1.34);
-        Vertex v2 = new Vertex("v2", 100, 50);
-        Edge e = new Edge("e", v1, v2);
-        System.out.println(v1.getDist(v1, v2));
-        System.out.println(e.getWeight());
+//        Vertex v1 = new Vertex("v1", 13.5, 1.34);
+//        Vertex v2 = new Vertex("v2", 100, 50);
+//        Edge e = new Edge("e", v1, v2);
+//        System.out.println(v1.getDist(v1, v2));
+//        System.out.println(e.getWeight());
     }
 }
