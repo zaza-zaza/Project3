f5956993165533e25f38a7f33fc0e75eac30b60b
zach <zgarson@u.rochester.edu>
2023-12-09 00:51:48 -0500

    can see the graph but that is about it. Still can't scale the graph correctly to see the points

--- src/Map.java
+++ src/Map.java
@@ -6,87 +6,94 @@ import java.awt.geom.Point2D;
 
 public class Map extends JFrame {
 
-    private MyArrayList<Vertex> vertices;
+    private MyArrayList<Vertex> coordinates; // Latitude and longitude coordinates
 
-    public Map (MyArrayList<Vertex> vertices){
-        this.vertices = vertices;
+    public Map(MyArrayList<Vertex> coordinates) {
+        super("GeoGraph Drawer");
 
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(1920, 1080);
+        this.coordinates = coordinates;
+
+        int w = 900;
+        int h = 900;
+
+
+        // Set up the frame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(900, 900);
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
-    private void drawGraph(Graphics2D g2d){
+    private void drawGeoGraph(Graphics2D g2d) {
+        // Draw edges
+        for (int i = 0; i < coordinates.size() - 1; i++) {
+            Vertex v1 = coordinates.get(i);
+            Vertex v2 = coordinates.get(i + 1);
 
-        double minX = Double.MAX_VALUE;
-        double minY = Double.MAX_VALUE;
-
-        for(Vertex v : vertices){
-            minX = Math.min(minX, v.getLatitude());
-            minY = Math.min(minY, v.getLongitude());
+            // Draw edge between coordinates i and i + 1
+            drawEdge(g2d, v1, v2, 900, 900);
         }
 
-        int translateX = (int) (200);
-        int translateY = (int) (200);
-
-        g2d.translate(translateX, translateY);
-
-        for(int i = 0; i < vertices.size() - 1; i++){
-            drawEdge(g2d, vertices.get(i), vertices.get(i + 1), minX, minY);
+        // Draw vertices
+        for (Vertex coordinate : coordinates) {
+            // Draw vertex at the coordinate
+            drawVertex(g2d, coordinate);
         }
-
-        for(int i = 0; i < vertices.size(); i++){
-            drawVertex(g2d, vertices.get(i), minX, minY);
-        }
-
     }
 
-    private void drawEdge(Graphics2D g2d, Vertex v1, Vertex v2, double minX, double minY){
+    private void drawEdge(Graphics2D g2d, Vertex v1, Vertex v2, int w, int h) {
+        g2d.setColor(Color.BLACK);
 
-        double x1 = (v1.getLatitude() - minX);
-        double y1 = (v1.getLongitude() - minY);
-        double x2 = (v2.getLatitude() - minX);
-        double y2 = (v2.getLongitude() - minY);
+        // Assuming simple scaling for illustration purposes
+//        int x1 = (int) (coord1.getX() * 1);
+//        int y1 = (int) (coord1.getY() * 1);
+//        int x2 = (int) (coord2.getX() * 1);
+//        int y2 = (int) (coord2.getY() * 1);
+
+        double scaleX = w / (v2.getLatitude() - v1.getLongitude());
+        double x1 = v1.getLatitude();
+        double y1 = v1.getLongitude();
+        double x2 = v2.getLatitude();
+        double y2 = v2.getLongitude();
 
         Line2D.Double l = new Line2D.Double(x1, y1, x2, y2);
         g2d.draw(l);
+//        g2d.drawLine(x1, y1, x2, y2);
     }
 
-    private void drawVertex(Graphics2D g2d, Vertex v, double minX, double minY){
+    private void drawVertex(Graphics2D g2d, Vertex coordinate) {
         g2d.setColor(Color.BLUE);
 
-        double x = (v.getLatitude() - minX);
-        double y = (v.getLongitude() - minY);
+        // Assuming simple scaling for illustration purposes
 
-
-        Ellipse2D.Double point = new Ellipse2D.Double(x - 5,
-                y - 5, 5, 5);
-        g2d.fill(point);
-
+        double x = coordinate.getLatitude();
+        double y = coordinate.getLongitude();
+        // Draw a circle to represent the vertex
+        Ellipse2D.Double ellipse = new Ellipse2D.Double(x - 5, y - 5, 10, 10);
+        g2d.fill(ellipse);
     }
 
-    public void paint(Graphics g){
+    @Override
+    public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        drawGraph(g2d);
+        drawGeoGraph(g2d);
     }
 
-    public static void main(String[] args){
-        MyArrayList<Vertex> vList = new MyArrayList<>();
-        Vertex a = new Vertex("a", 10.1, -300.0);
-        Vertex b = new Vertex("b", 30.0, -100.0);
-        Vertex c = new Vertex("c", 300.0, -50.0);
-        Vertex d = new Vertex("d", 30.0, -500.0);
+    public static void main(String[] args) {
+        // Example coordinates for a simple geographical graph
+        MyArrayList<Vertex> coordinates = new MyArrayList<>();
+        coordinates.add(new Vertex("a", 43.129807, 770.62742));
+        coordinates.add(new Vertex("b", 43.12966, 77.628598));
+        coordinates.add(new Vertex("c", 43.130478, 77.626726));
+        coordinates.add(new Vertex("d",43.131081, 77.62623));
+        coordinates.add(new Vertex("e", 43.131338, 77.625693));
+        coordinates.add(new Vertex("f", 100.0, 200.0));
+//        coordinates.add(new Vertex(100.0, 300.0));
+//        coordinates.add(new Vertex(300.0, 250.0));
+//        coordinates.add(new Ver)
 
 
-        vList.add(a);
-        vList.add(b);
-        vList.add(c);
-        vList.add(d);
-        SwingUtilities.invokeLater(() -> new Map(vList));
-
-
+        SwingUtilities.invokeLater(() -> new Map(coordinates));
     }
-
 }
